75124f90e24f7e2e54f4540ec992cfe444ce588e
matheushalas <matheushalas@gmail.com>
2020-09-12 12:45:32 -0300

    testando bloqueio de links, deu certo!

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) { return { cancel: true }},
-    { urls: []},
+    { urls: 
+        ["*://*.reddit.com/*","*://*.youtube.com/*","*://*.wallpapershome.com/*"]},
     ["blocking"]
 )
 
