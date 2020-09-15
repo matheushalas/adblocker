1d4098214c1ce7a078826a6d1beec4e580fa6c08
matheushalas <matheushalas@gmail.com>
2020-09-15 11:08:52 -0300

    testando remote

--- background.js
+++ background.js
@@ -3,6 +3,7 @@ defaultFilters = [
     "*://*.reddit.com/*",
     "*://*.youtube.com/*",
     "*://*.wallpapershome.com/*",
+    "*://*.tpc.googlesyndication.com/daca_images/simgad/14665391136553537509/*",
 ] 
 
 chrome.webRequest.onBeforeRequest.addListener(
